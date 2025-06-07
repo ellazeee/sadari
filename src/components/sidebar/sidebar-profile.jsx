5fe7c807a70c923a233107e4ecdc42f365a2dfba
Ellazee <elvirazaretti@gmail.com>
2025-06-07 15:32:38 +0700

    final

--- src/components/sidebar/sidebar-profile.jsx
+++ src/components/sidebar/sidebar-profile.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export const SidebarProfile = () => {
   return (
     <div class="flex flex-1 items-start gap-4">
-        <img src="../../public/cover-article.jpg" 
+        <img src="../../../public/cover-article.jpg" 
         alt="Profile" 
         class="rounded-full w-8 h-8"/>
         <h1 class="my-auto text-sm">Your Name</h1>
